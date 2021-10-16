af2e812624762dfd23d17b74adca2598103e3355
NurKeinNeid <mralexman3000@gmail.com>
2021-10-16 19:34:38 +0200

    sepolicy: Adapt to DerpFest
    Change-Id: Icc63869779e98ef592a7f1d50a5ddbffeb4aa6ab
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -1,6 +1,6 @@
 #
 # This policy configuration will be used by all products that
-# inherit from Lineage
+# inherit from DerpFest
 #
 
 ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
@@ -10,25 +10,25 @@ endif
 endif
 
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/public
+    device/derp/sepolicy/common/public
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/private
+    device/derp/sepolicy/common/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/dynamic \
-    device/lineage/sepolicy/common/system
+    device/derp/sepolicy/common/dynamic \
+    device/derp/sepolicy/common/system
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/dynamic \
-    device/lineage/sepolicy/common/vendor
+    device/derp/sepolicy/common/dynamic \
+    device/derp/sepolicy/common/vendor
 endif
 
 # Selectively include legacy rules defined by the products
--include device/lineage/sepolicy/legacy-common/sepolicy.mk
+-include device/derp/sepolicy/legacy-common/sepolicy.mk
 
 # Include atv rules on atv product
 ifeq ($(PRODUCT_IS_ATV), true)
-include device/lineage/sepolicy/atv/sepolicy.mk
+include device/derp/sepolicy/atv/sepolicy.mk
 endif
--- legacy-common/sepolicy.mk
+++ legacy-common/sepolicy.mk
@@ -1,9 +1,9 @@
 #
 # This policy configuration will be used by select legacy products that
-# inherit from Lineage
+# inherit from DerpFest
 #
 
 ifeq ($(TARGET_HAS_LEGACY_CAMERA_HAL1), true)
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/legacy-common/public/legacy-camera-hal1
+    device/derp/sepolicy/legacy-common/public/legacy-camera-hal1
 endif
